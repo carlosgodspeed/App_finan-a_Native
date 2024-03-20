5d2c871fa4360c8bf617515e5f8d65ff45180677
carlosgodspeed <carlosminibics@gmail.com>
2024-03-19 23:45:45 -0300

    estilizando a tela de SignIn

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,21 +1,48 @@
 import React from "react";
 import { View, Text } from "react-native";
-import { Background, Container, Logo, AreaInput, Input } from "./styles";
+
+import {
+   Background,
+   Container,
+   Logo,
+   AreaInput,
+   Input,
+   SubmitButton,
+   SubmitText,
+   Link,
+   LinkText
+} from "./styles";
 
 export default function SignIn() {
   return (
     <Background>
 
       <Container>
+
         <Logo
         source={require("../../assets/Logo.png")} 
         />
 
         <AreaInput>
         <Input
-        placeholder="Seu email"
+          placeholder="Seu email"
         />
         </AreaInput>
+
+        <AreaInput>
+        <Input
+          placeholder="Sua Senha"
+        />
+        </AreaInput>
+
+        <SubmitButton>
+          <SubmitText>Acessar</SubmitText>
+        </SubmitButton>
+
+        <Link>
+          <LinkText>Criar uma Conta</LinkText>
+        </Link>
+
       </Container>
 
     </Background>
