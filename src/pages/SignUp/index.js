00d7905aa120f26a0d6b5989ef283172eb0662c5
carlosgodspeed <carlosminibics@gmail.com>
2024-03-20 23:28:54 -0300

    Tela de cadastro criada

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,10 +1,47 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Platform } from 'react-native';
 
-export default function SignUn(){
-    return(
-        <View>
-            <Text>Tela cadastro</Text>
-        </View>
-    )
+import { 
+  Background, 
+  Container, 
+  AreaInput, 
+  Input, 
+  SubmitButton, 
+  SubmitText
+} from '../SignIn/styles';
+
+export default function SignUp(){
+  return(
+    <Background>
+      <Container
+        behavior={Platform.OS === 'ios' ? 'padding' : ''}
+        enabled
+      >
+
+        <AreaInput>
+          <Input
+            placeholder="Nome"
+          />
+        </AreaInput>
+
+        <AreaInput>
+          <Input
+            placeholder="Seu email"
+          />
+        </AreaInput>
+
+        <AreaInput>
+          <Input
+            placeholder="Sua senha"
+          />
+        </AreaInput>
+
+        <SubmitButton>
+          <SubmitText>Cadastrar</SubmitText>
+        </SubmitButton>
+
+      </Container>
+
+    </Background>
+  )
 }
