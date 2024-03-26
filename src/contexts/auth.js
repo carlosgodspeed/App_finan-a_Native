69cef81c39cded7e24d2299dd75ee92a86591412
carlosgodspeed <carlosminibics@gmail.com>
2024-03-26 20:57:10 -0300

    api adicionada e cadastro de usuarios configurados

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,5 +1,8 @@
 import React, { createContext, useState } from 'react';
 
+import api from '../services/api';
+import { useNavigation } from '@react-navigation/native';
+
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }){
@@ -7,12 +10,30 @@ function AuthProvider({ children }){
     nome: 'Usuario Teste'
   }); 
 
+  const navigation = useNavigation();
+
+
+  async function signUp(email, password, nome){
+    try{
+      const response = await api.post('/users', {
+       name: nome,
+       password: password,
+       email: email,
+      })
+
+      navigation.goBack();
+
+
+    }catch(err){
+      console.log("ERRO AO CADASTRAR", err)
+    }
+  }
+
   return(
-    <AuthContext.Provider value={{ user }}>
+    <AuthContext.Provider value={{ user, signUp }}>
       {children}
     </AuthContext.Provider>
   )
 }
 
 export default AuthProvider;
-
