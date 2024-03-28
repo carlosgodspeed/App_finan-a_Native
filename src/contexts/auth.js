e6f7d8f5ad4548e01cbb4f652c4ada483fc495df
carlosgodspeed <carlosminibics@gmail.com>
2024-03-27 21:19:25 -0300

    criação da Tela Home e adicionada animação de loading ao se cadastrar

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,36 +1,35 @@
 import React, { createContext, useState } from 'react';
-
 import api from '../services/api';
 import { useNavigation } from '@react-navigation/native';
 
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }){
-  const [user, setUser] = useState({
-    nome: 'Usuario Teste'
-  }); 
-
+  const [user, setUser] = useState(null);
+  const [loadingAuth, setLoadingAuth] = useState(false);
   const navigation = useNavigation();
 
-
   async function signUp(email, password, nome){
+    setLoadingAuth(true);
+
     try{
       const response = await api.post('/users', {
        name: nome,
        password: password,
        email: email,
       })
+      setLoadingAuth(false);
 
       navigation.goBack();
 
-
     }catch(err){
       console.log("ERRO AO CADASTRAR", err)
+      setLoadingAuth(false);
     }
   }
 
   return(
-    <AuthContext.Provider value={{ user, signUp }}>
+    <AuthContext.Provider value={{ user, signUp, loadingAuth }}>
       {children}
     </AuthContext.Provider>
   )
