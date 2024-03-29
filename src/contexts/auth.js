d21dbea715ae6b1f58a5c9f6675a9df20e9c606a
carlosgodspeed <carlosminibics@gmail.com>
2024-03-28 21:32:11 -0300

    criando função de login

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,38 +1,41 @@
-import React, { createContext, useState } from 'react';
-import api from '../services/api';
-import { useNavigation } from '@react-navigation/native';
+import React, { createContext, useState } from "react";
+import api from "../services/api";
+import { useNavigation } from "@react-navigation/native";
 
 export const AuthContext = createContext({});
 
-function AuthProvider({ children }){
+function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [loadingAuth, setLoadingAuth] = useState(false);
   const navigation = useNavigation();
 
-  async function signUp(email, password, nome){
+  async function signUp(email, password, nome) {
     setLoadingAuth(true);
 
-    try{
-      const response = await api.post('/users', {
-       name: nome,
-       password: password,
-       email: email,
-      })
+    try {
+      const response = await api.post("/users", {
+        name: nome,
+        password: password,
+        email: email,
+      });
       setLoadingAuth(false);
 
       navigation.goBack();
-
-    }catch(err){
-      console.log("ERRO AO CADASTRAR", err)
+    } catch (err) {
+      console.log("ERRO AO CADASTRAR", err);
       setLoadingAuth(false);
     }
   }
 
-  return(
-    <AuthContext.Provider value={{ user, signUp, loadingAuth }}>
+  async function signIn(email, password) {
+    console.log("EMAIL TESTE:", email)
+  }
+
+  return (
+    <AuthContext.Provider value={{ signed: !!user, user, signUp, signIn, loadingAuth }}>
       {children}
     </AuthContext.Provider>
-  )
+  );
 }
 
 export default AuthProvider;
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import { Platform } from 'react-native';
 
 import { 
@@ -14,9 +14,18 @@ import {
 } from './styles';
 
 import { useNavigation } from '@react-navigation/native';
+import { AuthContext } from '../../contexts/auth';
+
 
 export default function SignIn(){
   const navigation = useNavigation();
+  const { signIn, loadingAuth } = useContext(AuthContext);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleLogin(){
+    signIn();
+  }
 
   return(
     <Background>
@@ -32,16 +41,20 @@ export default function SignIn(){
         <AreaInput>
           <Input
             placeholder="Seu email"
+            value={email}
+            onChangeText={ (text) => setEmail(text) }
           />
         </AreaInput>
 
         <AreaInput>
           <Input
             placeholder="Sua senha"
+            value={password}
+            onChangeText={ (text) => setPassword(text) }
           />
         </AreaInput>
 
-        <SubmitButton activeOpacity={0.8}>
+        <SubmitButton activeOpacity={0.8} onPress={handleLogin}>
           <SubmitText>Acessar</SubmitText>
         </SubmitButton>
 
