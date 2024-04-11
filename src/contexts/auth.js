5f3295502b01089624a3e7921ef070b52df68d16
carlosgodspeed <carlosminibics@gmail.com>
2024-04-11 13:32:59 -0300

    registro de gastos concluido

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -8,10 +8,9 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }){
-  const [user, setUser] = useState(null); 
+  const [user, setUser] = useState(null);
   const [loadingAuth, setLoadingAuth] = useState(false);
   const [loading, setLoading] = useState(true);
-
   const navigation = useNavigation();
 
 
--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -3,10 +3,16 @@ import React, { useState } from 'react';
 import { Background, Input, SubmitButton, SubmitText } from './styles';
 import { SafeAreaView, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
 
-import Header from '../../components/Header'
-import RegisterTypes from '../../components/RegisterTypes'
+import Header from '../../components/Header';
+import RegisterTypes from '../../components/RegisterTypes';
+
+import api from '../../services/api';
+import { format } from 'date-fns';
+import { useNavigation } from '@react-navigation/native';
 
 export default function New(){
+  const navigation = useNavigation();
+
   const [labelInput, setLabelInput] = useState('');
   const [valueInput, setValueInput] = useState('');
   const [type, setType] = useState('receita');
@@ -22,8 +28,32 @@ export default function New(){
     Alert.alert(
       'Confirmando Dados',
       `Tipo: ${type} - valor: ${parseFloat(valueInput)}`,
-      []
+      [
+        {
+          text: 'cancelar',
+          style: 'cancel'
+        },
+        {
+          text:'Continuar',
+          onPress: () => handleAdd()
+        }
+      ]
     )
+  }
+
+  async function handleAdd(){
+    Keyboard.dismiss();
+
+    await api.post('/receive', {
+      description: labelInput,
+      value: Number(valueInput),
+      type: type,
+      date: format(new Date(), 'dd/MM/yyyy')
+    })
+
+    setLabelInput('');
+    setValueInput('');
+    navigation.navigate('Home');
   }
 
   return(
